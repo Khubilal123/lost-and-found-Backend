8bcc5201a07aa2b3b7066ea40e6ff63d1c991fb1
Khubilal123 <Khubilaljavat@gmail.com>
2025-11-28 18:31:48 +0530

    Fix import path for items route

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import itemsRouter from "./routes/items.js";
+import itemsRouter from "./src/routes/items.js";
 
 dotenv.config();
 
