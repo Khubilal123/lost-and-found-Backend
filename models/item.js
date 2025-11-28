6ea822f8fe86f67bab9bfdcc11ddf6e405546725
Khubilal123 <Khubilaljavat@gmail.com>
2025-11-28 23:03:21 +0530

    fixed undefined cntact and location info

--- models/item.js
+++ models/item.js
@@ -3,7 +3,10 @@ import mongoose from "mongoose";
 const itemSchema = new mongoose.Schema({
   name: String,
   description: String,
-  imageUrl: String,
+  location: String,
+  contact: String,
+  type: String, // lost or found
+  image: String, // matches your controller
   date: { type: Date, default: Date.now }
 });
 
